220fc9caa1a388d4e404ecc302291c798975a980
shotatuchiya <121003827+shotatuchiya@users.noreply.github.com>
2023-01-16 23:05:01 +0900

    モデル追記、ログイン状態の条件分岐

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -13,6 +13,6 @@ class Exhibit < ApplicationRecord
   validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sender_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: "Input half-width characters" }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
 end
