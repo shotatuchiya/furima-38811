13e0c98f479adfb2e5777af476a1bd8f8fdd1004
shotatuchiya <121003827+shotatuchiya@users.noreply.github.com>
2023-01-17 13:37:15 +0900

    user テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
+  describe 'ユーザー新規登録' do
     context '新規登録できるとき' do
       it 'nickname,email,passwordとpassword_confirmation,first_name,last_name,first_name_kana,last_name_kana,birthdayが存在すれば登録できる' do
         expect(@user).to be_valid
